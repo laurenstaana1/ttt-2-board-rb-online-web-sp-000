e75b3d10f14c2836884dcd4312e6d0da72d0f21d
Lauren Sta Ana <lauren.staana1@gmail.com>
2019-04-07 19:30:37 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,29 @@
-# Define the variable board below.
+board="variable"
+board=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Position 1: Left
+row [0]
+
+# Position 2: Middle
+row [0]
+
+# Position 3: Right
+row [0]
+
+# Position 4: Left
+row [1]
+
+#Position 5: Middle
+row [1]
+
+# Position 6: Right
+row [1]
+
+# Position 7: Left
+row [2]
+
+# Position 8: Middle
+row [2]
+
+# Position 9: Right
+row [2]
